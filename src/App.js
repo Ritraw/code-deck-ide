7f21513cdd4e133d58b777896afed50026b090d9
ritikrawat <RITIKRAWAT001@GMAIL.COM>
2022-11-13 00:20:41 +0530

    [Feature]: creating leftcomponent of home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter , Routes,Route} from 'react-router-dom'
+import Home from './screens/Home';
+import Playground from './screens/Playground';
+import Error404 from './screens/Error404';
+import { GlobalStyle } from './screens/style/global';
 
-function App() {
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <GlobalStyle/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/playground" element={<Playground/>} />
+        <Route path="*" element={<Error404/>} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/screens/Error404/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <div>NO PAGE FOUND</div>
+  )
+}
+
+export default Error404
